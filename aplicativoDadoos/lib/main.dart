be009595d81349ba51ca00c17a177827dd8a8ed2
Gabriel Silveira <109250251+silgabriell@users.noreply.github.com>
2023-10-05 23:39:42 -0300

    projeto dadoos

--- aplicativoDadoos/lib/main.dart
+++ aplicativoDadoos/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,53 @@ void main() {
   );
 }
 
-class Dadoos extends StatelessWidget {
+class Dadoos extends StatefulWidget {
+  @override
+  _DadoosState createState() => _DadoosState();
+}
+
+class _DadoosState extends State<Dadoos> {
+
+  int numeroDadoEsquerdo = 1;
+  int numeroDadoDireito = 1;
+
+  void alterarFacesDosDados () {
+    setState(() {
+      numeroDadoEsquerdo = Random().nextInt(6) + 1;
+      numeroDadoDireito = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  alterarFacesDosDados();
+                });
+              },
+              child: Image.asset('imagens/dado$numeroDadoEsquerdo.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  alterarFacesDosDados();
+                });
+              },
+              child: Image.asset('imagens/dado$numeroDadoDireito.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
+
